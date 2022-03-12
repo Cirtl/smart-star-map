b2770c31e6e91ff06c6f3cea024aab74a2986e26
Cirtl <2376846435@qq.com>
2022-03-12 17:44:52 +0800

    init the project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Viewer, CzmlDataSource } from 'resium';
+import czml from 'czml-writer';
+import _ from 'lodash';
+const fs = require('fs');
 
-function App() {
+const App = () => {
+  let satelliteData = [];
+  // add 5 dummy satellites
+  for (let index = 0; index < 5; index++) {
+    const newSatellite = {
+      apogee: _.random(400.0, 500.0), // km
+      perigee: _.random(400.0, 500.0), // km
+      inclination: _.random(0.0, 90.0), // deg
+      rightAscension: _.random(0.0, 360.0), // deg
+      argumentOfPeriapsis: _.random(0.0, 360.0), // deg
+    };
+    const newOrbit = new czml.orbit.fromParams(newSatellite);
+    const newCzml = newOrbit.czml();
+
+    const formattedName = `Satellite-${index + 1}`;
+
+    if (index === 0) {
+      newCzml[1].id = formattedName;
+      newCzml[1].label.text = formattedName;
+      satelliteData = [...satelliteData, ...newCzml];
+    }
+    newCzml[1].id = formattedName;
+    newCzml[1].label.text = formattedName;
+    satelliteData = [...satelliteData, newCzml[1]];
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Viewer shouldAnimate full>
+      <CzmlDataSource data={satelliteData} />
+    </Viewer>
   );
-}
-
+};
 export default App;
